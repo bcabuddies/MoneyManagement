e60d6d20c1d757f27f5ecd78d61abc2b49c8025a
Rahul Gaur <rahul.gaur152@gmail.com>
2019-07-09 22:13:29 +0530

    working on postRegistration Added TODO in notes.txt Firebase database exception

--- app/src/main/java/com/bcabuddies/moneymanagement/PostRegistration/View/PostRegistration.java
+++ app/src/main/java/com/bcabuddies/moneymanagement/PostRegistration/View/PostRegistration.java
@@ -2,13 +2,21 @@ package com.bcabuddies.moneymanagement.PostRegistration.View;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.bcabuddies.moneymanagement.Home.View.Home;
+import com.bcabuddies.moneymanagement.PostRegistration.Presenter.PostRegPresenter;
+import com.bcabuddies.moneymanagement.PostRegistration.Presenter.PostRegPresenterImpl;
 import com.bcabuddies.moneymanagement.R;
+import com.bcabuddies.moneymanagement.utils.Utils;
 import com.bumptech.glide.Glide;
 import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.Objects;
 
@@ -25,9 +33,12 @@ public class PostRegistration extends AppCompatActivity implements PostRegView {
     TextInputLayout postEmailLayout;
     @BindView(R.id.post_nextBtn)
     Button postNextBtn;
-    @BindView(R.id.circleImageView)
-    CircleImageView circleImageView;
+    @BindView(R.id.post_profileView)
+    CircleImageView postProfileView;
+
+    private PostRegPresenter postRegPresenter;
     private Bundle bundle;
+    private final String TAG = "PostReg.java";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,8 +52,18 @@ public class PostRegistration extends AppCompatActivity implements PostRegView {
             Objects.requireNonNull(postNameLayout.getEditText()).setText(Objects.requireNonNull(bundle.get("name")).toString());
             Objects.requireNonNull(postEmailLayout.getEditText()).setText(Objects.requireNonNull(bundle.get("email")).toString());
             Glide.with(this).load(Objects.requireNonNull(bundle.get("profile")).toString())
-                    .into(circleImageView);
+                    .into(postProfileView);
         }
+
+        FirebaseFirestore firebaseFirestore = FirebaseFirestore.getInstance();
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+        FirebaseUser user = auth.getCurrentUser();
+
+        postRegPresenter = new PostRegPresenterImpl(firebaseFirestore, user);
+        postRegPresenter.attachView(this);
+
+        //check if user is admin
+        postRegPresenter.checkAdmin(Objects.requireNonNull(bundle.get("email")).toString());
     }
 
     @Override
@@ -52,5 +73,32 @@ public class PostRegistration extends AppCompatActivity implements PostRegView {
 
     @OnClick(R.id.post_nextBtn)
     public void onViewClicked() {
+        //add user data to Database
+        postRegPresenter.uploadData(bundle);
+    }
+
+    @Override
+    public void errorMsg(String error) {
+        //some error
+        Utils.showMessage(this, error);
+        Log.e(TAG, "errorMsg: error " + error);
+    }
+
+    @Override
+    public void uploadSuccess() {
+        Log.e(TAG, "uploadSuccess: success ");
+        Utils.setIntentNoBackLog(this, Home.class);
+    }
+
+    @Override
+    public void userIsNotAdmin() {
+        //user is not admin
+        Log.e(TAG, "userIsNotAdmin: user is not admin ");
+    }
+
+    @Override
+    public void userIsAdmin() {
+        //user is admin
+        Log.e(TAG, "userIsAdmin: user is admin");
     }
 }

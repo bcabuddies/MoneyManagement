d564303c1e431137abb34f9a3cc50941165ebc59
Rahul Gaur <rahul.gaur152@gmail.com>
2019-07-25 00:22:37 +0530

    changed layout of Home and added ViewUser layout now working on Encryption and Decryption

--- app/src/main/java/com/bcabuddies/moneymanagement/Home/View/Home.java
+++ app/src/main/java/com/bcabuddies/moneymanagement/Home/View/Home.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -40,19 +41,21 @@ public class Home extends AppCompatActivity implements HomeView {
     RecyclerView homeRecyclerView;
     @BindView(R.id.home_cardView)
     CardView homeCardView;
-    private final String TAG = "Home.java";
-    @BindView(R.id.home_profileView)
-    CircleImageView homeProfileView;
-    @BindView(R.id.home_nameView)
-    TextView homeNameView;
     @BindView(R.id.home_logOut_btn)
     Button homeLogOutBtn;
-
+    private final String TAG = "Home.java";
+    @BindView(R.id.home_toolbar_user_name)
+    TextView homeToolbarUserName;
+    @BindView(R.id.home_toolbar_menuImage)
+    ImageView homeToolbarMenuImage;
+    @BindView(R.id.home_toolbar_searchImage)
+    ImageView homeToolbarSearchImage;
+    @BindView(R.id.home_toolbar_userImg)
+    CircleImageView homeToolbarUserImg;
     private FirebaseUser user;
     private FirebaseFirestore firestore;
     private ArrayList<UserModel> userList;
     private HomeAdapter adapter;
-    private HomePresenter presenter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -71,9 +74,9 @@ public class Home extends AppCompatActivity implements HomeView {
                 String name = task.getResult().getString("name");
                 String profile = task.getResult().getString("profile");
 
-                homeNameView.setText(name);
+                homeToolbarUserName.setText(name);
                 try {
-                    Glide.with(this).load(profile).into(homeProfileView);
+                    Glide.with(this).load(profile).into(homeToolbarUserImg);
                 } catch (Exception e) {
                     e.printStackTrace();
                     Log.e(TAG, "showNameAndProfile: error loading profile " + e.getMessage());
@@ -83,7 +86,7 @@ public class Home extends AppCompatActivity implements HomeView {
     }
 
     private void init() {
-        presenter = new HomePresenterImp();
+        HomePresenter presenter = new HomePresenterImp();
         presenter.attachView(this);
 
         user = FirebaseAuth.getInstance().getCurrentUser();
@@ -102,7 +105,7 @@ public class Home extends AppCompatActivity implements HomeView {
         return this;
     }
 
-    @OnClick({R.id.home_add_user_btn, R.id.home_logOut_btn})
+    @OnClick({R.id.home_add_user_btn, R.id.home_logOut_btn, R.id.home_toolbar_menuImage, R.id.home_toolbar_searchImage})
     public void onViewClicked(View view) {
         switch (view.getId()) {
             case R.id.home_add_user_btn:
@@ -111,7 +114,21 @@ public class Home extends AppCompatActivity implements HomeView {
             case R.id.home_logOut_btn:
                 Utils.googleSignOut(this);
                 break;
+            case R.id.home_toolbar_menuImage:
+                createMenu();
+                break;
+            case R.id.home_toolbar_searchImage:
+                openSearch();
+                break;
         }
+    }
+
+    private void createMenu() {
+
+    }
+
+    private void openSearch() {
+
     }
 
     @Override
